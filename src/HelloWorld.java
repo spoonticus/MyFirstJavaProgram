65eca830a8303e956e815f72c174956ecdc74475
spoonticus <olivia.r.doe@gmail.com>
2018-05-31 11:38:04 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println( "Hello, World!" );
+        System.out.println("Today is not July 19, 2015. I am alive!");
+        System.out.println("My name is Doe."); }
+}
